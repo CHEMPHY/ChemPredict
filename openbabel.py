80aeffd839150ad0be917ed5ae05be6bf288fc0a
Mengyuan Zhu <zhumengyuan@outlook.com>
2016-02-08 15:34:00 -0500

    rxn file support

--- openbabel.py
+++ openbabel.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import sys
+import os
+import glob
 from rdkit.Chem import AllChem
 from rdkit import Chem
 
@@ -13,16 +15,51 @@ molecule2=Chem.MolFromSmiles(smiles1)
 
 
 reactions=[]
-reactions.append('[C:1](=[O:2])-[OD1].[N!H0:3]>>[C:1](=[O:2])[N:3]')
-reactions.append('[C@H1:1][OH:2].[OH][C:3]=[O:4]>>[C@:1][O:2][C:3]=[O:4]')
-
+reactions.append('[C:1](=[O:2])-[OD1].[N!H0:3]>>[C:1](=[O:2])[N:3]')  #amination
+reactions.append('[C:1][OH:2].[OH][C:3]=[O:4]>>[C:1][O:2][C:3]=[O:4]') #esterification
 
 for reaction in reactions:	
 	rxn = AllChem.ReactionFromSmarts(reaction)
 	ps = rxn.RunReactants((molecule1,molecule2))
 	if len(ps)!=0:
-		print Chem.MolToSmiles(ps[0][0],True)
+		uniqps={}
+		for product in ps:
+			smi=Chem.MolToSmiles(product[0],True)
+			uniqps[smi]=product[0]
+		for key in uniqps:
+			print key
+
 	ps = rxn.RunReactants((molecule2,molecule1))
 	if len(ps)!=0:
-		print Chem.MolToSmiles(ps[0][0],True)	
+		uniqps={}
+		for product in ps:
+			smi=Chem.MolToSmiles(product[0],True)
+			uniqps[smi]=product[0]
+		for key in uniqps:
+			print key
 
+
+'''
+
+i=0
+os.chdir("/home/mzhu7/chemroutes/rxn")
+for file in glob.glob("*.rxn"):
+	#print file
+	try:
+		rxn=AllChem.ReactionFromRxnFile(file)
+		if rxn.GetNumReactantTemplates()==2:			
+			#print i	
+			ps = rxn.RunReactants((molecule1,molecule2))				
+			i=i+1
+			if len(ps)!=0:
+				for i in range(0, len(ps)):
+					print Chem.MolToSmiles(ps[0][0],True)	
+			ps = rxn.RunReactants((molecule2,molecule1))
+			if len(ps)!=0:
+				for i in range(0, len(ps)):
+					print Chem.MolToSmiles(ps[0][0],True)
+	except:
+		pass
+
+
+'''	
