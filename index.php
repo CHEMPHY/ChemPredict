862ab7f763ab0db6939d0c5c06725ac7c9cb9885
Mengyuan Zhu <zhumengyuan@outlook.com>
2016-02-09 13:42:53 -0500

    banner.png

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
+<div style="margin:0;"> 
+<span style="font-size:90px;">Chempredict</span>
+<img style="height:100px;" src="images/banner.png" alt="chempredict" />
+</div>
+
+
+<br />
 
 
 <form action="submit.php" method="post" enctype="multipart/form-data" >
-<input type="file" name="file1">
-<input type="file" name="file2">
-
+File1: <input type="file" name="file1"> <br />
+File2: <input type="file" name="file2"> <br />
 <input type="submit">
-
-
-
-
 </form>
 
 
