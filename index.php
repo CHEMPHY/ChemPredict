566a2a563603981be44c60f3ae371c0ec2b2e684
Mengyuan Zhu <zhumengyuan@outlook.com>
2016-03-27 18:59:55 -0400

    fix reactants number problem

--- index.php
+++ index.php
@@ -13,70 +13,8 @@
         </title>
         <link rel="stylesheet" href="bootstrap.min.css"/>
         <link href="cover.css" rel="stylesheet"/>
-
-        <script src="marvinjs/js/lib/rainbow/rainbow-custom.min.js"></script>
-        <script src="marvinjs/gui/lib/promise-1.0.0.min.js"></script>
-        <script src="marvinjs/js/marvinjslauncher.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="bootstrap.min.js"></script>
-        <script src="eModal.min.js">
-        </script>
-
-        <script>
-            $( "#num_reactants" ).change(function() {
-            $("#files").empty();
-            var i;
-            var fileindex;
-            for(i=1;i
-            <=this.value;i++){
-                fileindex="file"+i;
-                $("#files").append("<input type='file' name="+fileindex+" /><br/>");
-                }
-                console.log(this.value)
-                });
-            
-        </script>
-
-        <script>
-            window.addEventListener("message", function(event) {
-            try {
-            var externalCall = JSON.parse(event.data);
-            marvin.onReady(function() {
-            marvin.sketcherInstance[externalCall.method].apply(marvin.sketcherInstance, externalCall.args);
-            });
-            } catch (e) {
-            console.log(e);
-            }
-            }, false);
-            // called when Marvin JS loaded
-            function sketchOnLoad() {
-            if(marvin.Sketch.isSupported()) {
-            marvin.sketcherInstance = new marvin.Sketch("sketch");
-            marvin.sketcherInstance.setServices(getDefaultServices());
-            } else {
-            alert("Cannot initiate sketcher. Current browser may not support HTML canvas or may run in Compatibility Mode.");
-            }
-            }
-        </script>     
-
-		<script>
-		function myFunction()
-		{
-			eModal.iframe("test.htm" ,"Structures");
-
-		}
-
-		 function hello(string){
-   var name=string
-   document.getElementById('smiles1').value=name;
-   }
-		</script>
-
     </head>
     <body>
-
-
-
         <div class="site-wrapper">
             <div class="site-wrapper-inner">
                 <div class="cover-container">
@@ -130,37 +68,29 @@
                                     </select>
                                 </fieldset>
                                 <div id="files">
-                                Reactant1: 
+                                Reactant/Reagent 1: 
                                     <label class="btn btn-success btn-sm" for="my-file-selector" style="width:150px;">
                                         <input name="file1" id="my-file-selector" type="file" style="display:none;"  />
                                         Upload
-                                    </label>
-                                    
+                                    </label>                                    
                                     <button type="button" class="btn btn-warning btn-sm" id="file1draw" onclick="myFunction('smiles1')"  style="width:150px;">
                                         Draw
                                     </button>
-
                                     <input type=hidden name="smiles1" id="smiles1" />
-
-                                
-                                   
-
-                                    <br/><br/> Reactant2: 
+                                    <br/><br/> 
+                                Reactant/Reagent 2: 
                                     <label class="btn btn-success btn-sm" for="my-file-selector2" style="width:150px;">
                                         <input name="file2" id="my-file-selector2" type="file" style="display:none;" />
                                         Upload
-                                    </label>
-                                    
+                                    </label>                                    
                                     <button type="button" class="btn btn-warning btn-sm" onclick="myFunction('smiles2')"  style="width:150px;">
                                         Draw
                                     </button>
                                     <input type=hidden name="smiles2" id="smiles2" />
-
-                                   
-                                    
+                                    <br/><br/>
                                 </div>
 
-                                <br/>
+                                
                                 <button type="submit" class="btn btn-primary btn-lg" style="width:200px;">
                                     Submit
                                 </button>
@@ -177,6 +107,69 @@
                 </div>
             </div>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="bootstrap.min.js"></script>
+
+        <script src="marvinjs/js/lib/rainbow/rainbow-custom.min.js"></script>
+        <script src="marvinjs/gui/lib/promise-1.0.0.min.js"></script>
+        <script src="marvinjs/js/marvinjslauncher.js"></script>        
+        
+        <script src="eModal.min.js"></script>
+       
+
+        <script>
+            window.addEventListener("message", function(event) {
+            try {
+            var externalCall = JSON.parse(event.data);
+            marvin.onReady(function() {
+            marvin.sketcherInstance[externalCall.method].apply(marvin.sketcherInstance, externalCall.args);
+            });
+            } catch (e) {
+            console.log(e);
+            }
+            }, false);
+            // called when Marvin JS loaded
+            function sketchOnLoad() {
+            if(marvin.Sketch.isSupported()) {
+            marvin.sketcherInstance = new marvin.Sketch("sketch");
+            marvin.sketcherInstance.setServices(getDefaultServices());
+            } else {
+            alert("Cannot initiate sketcher. Current browser may not support HTML canvas or may run in Compatibility Mode.");
+            }
+            }
+        </script>     
+
+		<script>
+		function myFunction()
+		{
+			eModal.iframe("modal.htm" ,"Structures");
+
+		}
+
+		function hello(string){
+   			var name=string
+   			document.getElementById('smiles1').value=name;
+  		}
+		</script>
+
+
+		<script>
+
+            $("#num_reactants").change(function() {
+            	console.log("sb")
+	            $("#files").empty();
+	            var i;
+	            var fileindex;
+	            for(i=1;i<=this.value;i++){
+	                fileindex="file"+i;
+	                $("#files").append("Reactant/Reagent "+i+": <label class='btn btn-success btn-sm' for=my-file-selector"+i+" style='width:150px;'><input name=file"+i+" id=my-file-selector"+i+" type='file' style='display:none;' />Upload</label> <button type='button' class='btn btn-warning btn-sm' onclick=\"myFunction(\'smiles"+i+"\')\"  style=\"width:150px;\">Draw</button><input type=hidden name=smiles"+i+" id=smiles"+i+" /><br/><br/>");
+	                }
+	               
+                
+             });
+
+                         
+        </script>
         <!--
             Bootstrap core JavaScript
             ==================================================
